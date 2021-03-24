92398080147d46999df4ef4d880872e81ec2ea83
Annika Stein <annika-stein@t-online.de>
2021-03-24 22:49:51 +0000

    Upload New File

---
+++ new_march_21/input_noise_plots_no1.sh
@@ -0,0 +1,33 @@
+#!/usr/local_rwth/bin/zsh
+
+#SBATCH --ntasks=2
+
+#SBATCH --ntasks-per-node=2
+
+#SBATCH --mem-per-cpu=8G
+
+#SBATCH --cpus-per-task=2
+
+#SBATCH --job-name=CP_NOISE_0
+
+#SBATCH --output=output.%J.txt
+
+#SBATCH --time=20:30:00
+
+#SBATCH --account=rwth0583
+
+# with gpu: 
+
+# SBATCH --gres=gpu:1
+
+#SBATCH --mail-type=ALL
+
+#SBATCH --mail-user=annika.stein@rwth-aachen.de
+
+cd /home/um106329/aisafety
+#module unload intelmpi; module switch intel gcc
+#module load cuda/110
+#module load cudnn
+source ~/miniconda3/bin/activate
+conda activate my-env
+python3 compare_weighted.py 0 11 'noise' 'yes'
