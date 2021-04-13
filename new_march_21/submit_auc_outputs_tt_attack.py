04b03267be4c467510dec1d7d4cc2ffe366e414c
Annika Stein <annika-stein@t-online.de>
2021-04-13 17:01:25 +0000

    Update submit_auc_outputs_tt_attack.py

--- new_march_21/submit_auc_outputs_tt_attack.py
+++ new_march_21/submit_auc_outputs_tt_attack.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-## file: submit_auc.py
+## file: submit_auc_outputs_tt_attack.py
 
 
 # ================== Following / adapted from https://www.osc.edu/book/export/html/4046 ====================
@@ -13,11 +13,13 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Setup for AUC for custom tagger outputs")
 parser.add_argument('-i',"--files", type=int, help="Number of files", default=1)
-parser.add_argument('-m',"--mode", type=str, help="Mode: raw, noise, FGSM", default="raw")
+parser.add_argument('-m',"--mode", type=str, help="Mode: raw, deepcsv, noise, FGSM", default="raw")
+parser.add_argument('-td',"--traindataset", type=str, help="Dataset used during training, qcd or tt", default="tt")
 args = parser.parse_args()
 
 NUM_DATASETS = args.files
 mode = args.mode
+traindataset = args.traindataset
 
 home = os.path.expanduser('~')
 logPath = home + "/aisafety/output_slurm"
@@ -27,7 +29,7 @@ print('Output of Slurm Jobs will be placed in:\t',logPath)
 shPath = home + "/aisafety/new_march_21/code/"
 print('Shell script is located at:\t',shPath)
 
-if mode == 'raw':
+if mode == 'raw' or mode == 'deepcsv':
     time = '01'
     params = ['0.0']
 elif mode == 'noise':
@@ -40,11 +42,11 @@ elif mode == 'FGSM':
 for param in params:
     
     submit_command = ("sbatch "
-            "--time={3}:00:00 "
-            "--job-name=AUCOutputs_{0}_{1}_{2} "
-            "--export=NUM_F={0},MODE={1},PARAM={2} {4}auc_outputs_tt_attack.sh").format(NUM_DATASETS, mode, param, time, shPath)
+            "--time={4}:00:00 "
+            "--job-name=AUCOutputs_{0}_{1}_{2}_{3} "
+            "--export=NUM_F={0},MODE={1},PARAM={2},TRAINDATASET={3} {5}auc_outputs_tt_attack.sh").format(NUM_DATASETS, mode, param, traindataset, time, shPath)
     
-    #print(submit_command)
+    print(submit_command)
     exit_status = subprocess.call(submit_command, shell=True)
     if exit_status==1:  # Check to make sure the job submitted
         print("Job {0} failed to submit".format(submit_command))
